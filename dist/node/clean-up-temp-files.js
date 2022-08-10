c67f8208e99fcc2b591e7caf422715008c904746
Birk Skyum <birk.skyum@pm.me>
2022-08-11 00:41:49 +0200

    commit the dist folder

---
+++ dist/node/clean-up-temp-files.js
@@ -0,0 +1,22 @@
+//      
+'use strict';
+
+const path = require('path');
+const del = require('del');
+
+const cleanUpTempFiles = (
+  batfishConfig                      
+)               => {
+  return del(
+    ['assets.json', 'static-render-pages.js', 'static-render-pages.js.map'],
+    {
+      force: true,
+      cwd: path.join(
+        batfishConfig.outputDirectory,
+        batfishConfig.publicAssetsPath
+      )
+    }
+  );
+};
+
+module.exports = cleanUpTempFiles;
